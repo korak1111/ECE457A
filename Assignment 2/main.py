d8c14cfacdd88c0b15524adf5655021eb604d141
korak1111 <71841158+korak1111@users.noreply.github.com>
2021-10-18 21:08:29 -0400

    Q2 Complete

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -1,169 +1,13 @@
 # Cyrus Rupa 2021 for ECE457A A#2
+import collections
+import math
+import numpy as np
+from queue import Queue
+from queue import LifoQueue
+import heapq
+from termcolor import colored
 
-import numpy as py
-import queue as queue
-import copy
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-def case_Zero(maze):
-    tempMaze = copy.deepcopy(maze)
-    tempMaze[6][5] = "S"
-    tempMaze[0][5] = "E"
-    start = [6, 5]
-    # end = [19, 23]
-    print_maze(tempMaze)
-    print("Case 0 S:(5,6) to E:(5,0)")
-    solve_BFS(tempMaze, start)
-    return
-
-
-def case_one(maze):
-    tempMaze = copy.deepcopy(maze)
-    tempMaze[11][2] = "S"
-    tempMaze[19][23] = "E"
-    print_maze(tempMaze)
-    start = [11, 2]
-    end = [19, 23]
-    print("Case 1 S:(3,12) to E:(24,20)")
-    solve_BFS(tempMaze, start)
-    return
-
-
-def case_Two(maze):
-    tempMaze = copy.deepcopy(maze)
-    tempMaze[11][2] = "S"
-    tempMaze[21][2] = "E"
-    start = [11, 2]
-    end = [21, 2]
-    print("Case 2: 3,12 to 3,22")
-
-    return
-
-
-def case_Three(maze):
-    tempMaze = copy.deepcopy(maze)
-    tempMaze[0][0] = "S"
-    tempMaze[24][24] = "E"
-    start = [0, 0]
-    end = [24, 24]
-    print("Case 3: 0,0 to 24,24")
-    solve_BFS(tempMaze, start)
-
-    return tempMaze
-
-
-def solve_BFS(maze, start):
-    nums = queue.Queue()
-    nums.put("")
-    add = ""
-    while not findEnd(maze, add, start):
-        add = nums.get()
-        for i in ["L", "R", "U", "D"]:
-            put = add + i
-            if validMove(maze, put, start):
-                nums.put(put)
-
-
-def findEnd(maze, moves, start):
-    i = start[0]
-    j = start[1]
-    # print(moves)
-    for move in moves:
-        if move == "L":
-            i -= 1
-        elif move == "R":
-            i += 1
-        elif move == "U":
-            j += 1
-        elif move == "D":
-            j -= 1
-    if maze[j][i] == "E":
-        print("Found it!:" + moves)
-        maze = addPath(maze, moves, start)
-        print_maze(maze)
-        return True
-
-
-def validMove(maze, moves, start):
-    i = start[0]
-    j = start[1]
-
-    for move in moves:
-        if move == "L":
-            i -= 1
-        elif move == "R":
-            i += 1
-        elif move == "U":
-            j += 1
-        elif move == "D":
-            j -= 1
-
-        if not (0 <= i < len(maze[0]) and 0 <= j < len(maze)):
-            return False
-        elif (maze[j][i] == "#"):
-            return False
-    return True
-
-
-def addPath(maze, moves, start):
-    i = start[0]
-    j = start[1]
-    for move in moves:
-        if move == "L":
-            i -= 1
-        elif move == "R":
-            i += 1
-        elif move == "U":
-            j += 1
-        elif move == "D":
-            j -= 1
-        maze[j][i] = "X"
-    return maze
-
-
-def print_maze(maze):
-    temp = []
-    for j in range(len(maze[0])):
-        temp.append(str(j))
-    print(temp)
-
-
-    for i in range(len(maze) - 1, -1, -1):
-        temp = []
-        for j in range(len(maze[0])):
-            if maze[i][j] == "S":
-                temp.append("S")
-            elif maze[i][j] == "E":
-                temp.append("E")
-            elif maze[i][j] == "X":
-                temp.append("X")
-            elif maze[i][j] == 0 or maze[i][j]==" ":
-                temp.append(" ")
-            elif maze[i][j] == 1 or maze[i][j] == "#":
-                temp.append("#")
-        print(temp, i)
-
-
-def createMaze():
-    maze = []
-    maze.append(["#", "#", "#", "#", "#", " ", "#"])
-    maze.append(["#", " ", " ", " ", "#", " ", "#"])
-    maze.append(["#", " ", "#", " ", "#", " ", "#"])
-    maze.append(["#", " ", "#", " ", " ", " ", "#"])
-    maze.append(["#", " ", "#", "#", "#", " ", "#"])
-    maze.append(["#", " ", " ", " ", "#", " ", "#"])
-    maze.append(["#", "#", "#", "#", "#", " ", "#"])
-    return maze
-
-
-def main():
-    # do a thing
-    maze1 = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+mazeList = ([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
@@ -187,16 +31,203 @@ def main():
              [0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
              [1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0]]
-    maze2 = createMaze()
-    case_Zero(maze2)
-    # case_one(maze1)
-    # case_Two(maze1)
-    # case_Three(maze1)
-
-    return 1
+             [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0]])
 
 
-if __name__ == '__main__':
-    print_hi("Cyrus")
-    main()
+class Maze:
+    def __init__(self, mazeArr, start, end):
+        self.mazeArr = mazeArr
+        self.start = start
+        self.end = end
+        self.visitedTiles = np.zeros((len(mazeArr), len(mazeArr[0])))
+        self.numNodesVisited = 0
+        self.cost = 0
+
+    def printMaze(self):
+        temp = []
+        print("Printing Maze:")
+        for i in range(len(self.mazeArr), -2, -1):
+            for j in range(-1, len(self.mazeArr[0]) + 1):
+                if i == -1 or i == 25:
+                    temp.append(colored("-", "red"))
+                elif j == -1 or j == 25:
+                    temp.append(colored("|", "red"))
+                elif i == self.start[0] and j == self.start[1]:
+                    temp.append(colored("S", "green"))
+                elif i == self.end[0] and j == self.end[1]:
+                    temp.append(colored("E", "red"))
+                elif self.visitedTiles[i][j] == -1:
+                    temp.append(colored("X", "green"))
+                    self.cost += 1
+                elif self.visitedTiles[i][j] == 2:
+                    temp.append(colored("*", "cyan"))
+                elif self.mazeArr[i][j] == 1:
+                    temp.append("#")
+                elif self.mazeArr[i][j] == 0:
+                    temp.append(" ")
+            print(*temp)
+            temp = []
+
+    def nodesVisited(self):
+        for i in range(len(self.visitedTiles)):
+            for j in range(len(self.visitedTiles[0])):
+                if self.visitedTiles[i][j] != 0:
+                    self.numNodesVisited += 1
+        return self.numNodesVisited
+
+
+class Node:
+    def __init__(self, x, y, distStart=0, parent=None, aStar=False):
+        self.x = x
+        self.y = y
+        self.distStart = distStart
+        self.parent = parent
+        self.aStar = aStar
+        self.estimate = math.sqrt(((end[0] - self.x) * 2) + ((end[1] - self.y) * 2))  # pythagorean distance to goal
+
+    def __lt__(self, other):
+        return True
+
+
+def addMoves(currentNode, Maze, queue, aStar=False):
+    if not aStar:
+        # Go Up?
+        # not at edge, not at wall, not already visited
+        if (not currentNode.x == 24) and Maze.mazeArr[currentNode.x + 1][currentNode.y] == 0 and (
+                Maze.visitedTiles[currentNode.x + 1][currentNode.y] == 0):
+            newNode = Node(currentNode.x + 1, currentNode.y, currentNode.distStart + 1, currentNode)
+            Maze.visitedTiles[currentNode.x + 1, currentNode.y] = 1  # Added to Queue
+            queue.put(newNode)
+        # Go down?
+        # not at edge, not at wall, not already visited
+        if (not currentNode.x == 0) and Maze.mazeArr[currentNode.x - 1][currentNode.y] == 0 and (
+                Maze.visitedTiles[currentNode.x - 1][currentNode.y] == 0):
+            newNode = Node(currentNode.x - 1, currentNode.y, currentNode.distStart + 1, currentNode)
+            Maze.visitedTiles[currentNode.x - 1, currentNode.y] = 1  # Added to Queue
+            queue.put(newNode)
+        # Go Right?
+        # not at edge, not at wall, not already visited
+        if (not currentNode.y == 24) and Maze.mazeArr[currentNode.x][currentNode.y + 1] == 0 and (
+                Maze.visitedTiles[currentNode.x][currentNode.y + 1] == 0):
+            newNode = Node(currentNode.x, currentNode.y + 1, currentNode.distStart + 1, currentNode)
+            Maze.visitedTiles[currentNode.x, currentNode.y + 1] = 1  # Added to Queue
+            queue.put(newNode)
+        # Go Left?
+        # not at edge, not at wall, not already visited
+        if (not currentNode.y == 0) and Maze.mazeArr[currentNode.x][currentNode.y - 1] == 0 and (
+                Maze.visitedTiles[currentNode.x][currentNode.y - 1] == 0):
+            newNode = Node(currentNode.x, currentNode.y - 1, currentNode.distStart + 1, currentNode)
+            Maze.visitedTiles[currentNode.x, currentNode.y - 1] = 1  # Added to Queue
+            queue.put(newNode)
+    else:
+        # Go Up?
+        # not at edge, not at wall, not already visited
+        if (not currentNode.x == 24) and Maze.mazeArr[currentNode.x + 1][currentNode.y] == 0 and (
+                Maze.visitedTiles[currentNode.x + 1][currentNode.y] == 0):
+            newNode = Node(currentNode.x + 1, currentNode.y, currentNode.distStart + 1, currentNode)
+            Maze.visitedTiles[currentNode.x + 1, currentNode.y] = 1  # Added to Queue
+            newNode.estimate = newNode.distStart + newNode.estimate
+            heapq.heappush(queue, (newNode.estimate, newNode))
+        # Go down?
+        # not at edge, not at wall, not already visited
+        if (not currentNode.x == 0) and Maze.mazeArr[currentNode.x - 1][currentNode.y] == 0 and (
+                Maze.visitedTiles[currentNode.x - 1][currentNode.y] == 0):
+            newNode = Node(currentNode.x - 1, currentNode.y, currentNode.distStart + 1, currentNode)
+            Maze.visitedTiles[currentNode.x - 1, currentNode.y] = 1  # Added to Queue
+            newNode.estimate = newNode.distStart + newNode.estimate
+            heapq.heappush(queue, (newNode.estimate, newNode))
+
+        # Go Right?
+        # not at edge, not at wall, not already visited
+        if (not currentNode.y == 24) and Maze.mazeArr[currentNode.x][currentNode.y + 1] == 0 and (
+                Maze.visitedTiles[currentNode.x][currentNode.y + 1] == 0):
+            newNode = Node(currentNode.x, currentNode.y + 1, currentNode.distStart + 1, currentNode)
+            Maze.visitedTiles[currentNode.x, currentNode.y + 1] = 1  # Added to Queue
+            newNode.estimate = newNode.distStart + newNode.estimate
+            heapq.heappush(queue, (newNode.estimate, newNode))
+
+        # Go Left?
+        # not at edge, not at wall, not already visited
+        if (not currentNode.y == 0) and Maze.mazeArr[currentNode.x][currentNode.y - 1] == 0 and (
+                Maze.visitedTiles[currentNode.x][currentNode.y - 1] == 0):
+            newNode = Node(currentNode.x, currentNode.y - 1, currentNode.distStart + 1, currentNode)
+            Maze.visitedTiles[currentNode.x, currentNode.y - 1] = 1  # Added to Queue
+            newNode.estimate = newNode.distStart + newNode.estimate
+            heapq.heappush(queue, (newNode.estimate, newNode))
+
+
+def getPath(currentNode, Maze):
+    path = []
+    while currentNode.parent != None:
+        path.append([currentNode.x, currentNode.y])
+        Maze.visitedTiles[currentNode.x, currentNode.y] = -1  # mark on path
+        currentNode = currentNode.parent
+    path.append([currentNode.x, currentNode.y])
+    revPath = path[::-1]
+    for i in revPath:
+        temp = i[0]
+        i[0] = i[1]
+        i[1] = temp
+    return revPath
+
+
+def BFS(Maze):
+    currentNode = Node(Maze.start[0], Maze.start[1])
+    while not (currentNode.x == Maze.end[0] and currentNode.y == Maze.end[1]):
+        Maze.visitedTiles[currentNode.x, currentNode.y] = 2
+        addMoves(currentNode, Maze, bfsQ)
+        currentNode = bfsQ.get()
+    # print("Path Found!")
+    bfsPath = getPath(currentNode, Maze)
+    return bfsPath
+
+
+def DFS(Maze):
+    currentNode = Node(Maze.start[0], Maze.start[1])
+    while not (currentNode.x == Maze.end[0] and currentNode.y == Maze.end[1]):
+        Maze.visitedTiles[currentNode.x, currentNode.y] = 2
+        addMoves(currentNode, Maze, dfsQ)
+        currentNode = dfsQ.get()
+    # print("Path Found!")
+    dfsPath = getPath(currentNode, Maze)
+    return dfsPath
+
+
+def aStarS(Maze):
+    currentNode = Node(Maze.start[0], Maze.start[1])
+    while not (currentNode.x == Maze.end[0] and currentNode.y == Maze.end[1]):
+        Maze.visitedTiles[currentNode.x, currentNode.y] = 2
+        addMoves(currentNode, Maze, aStarQ, True)
+        currentNode = heapq.heappop(aStarQ)[1]
+    # print("Path Found!")
+    aStarPath = getPath(currentNode, Maze)
+    return aStarPath
+
+
+print("Case 1 S:(3,12) to E:(24,20)")
+start = [11, 2]
+end = [19, 23]
+
+print("Case 2: 3,12 to 3,22")
+start = [11, 2]
+end = [21, 2]
+
+print("Case 3: 0,0 to 24,24")
+start = [0, 0]
+end = [24, 24]
+
+bfsQ = Queue()
+dfsQ = LifoQueue()
+aStarQ = []
+heapq.heapify(aStarQ)
+
+maze = Maze(mazeList, start, end)
+
+# path = BFS(maze)
+# path = DFS(maze)
+path = aStarS(maze)
+
+maze.printMaze()
+print("Cost: ", maze.cost)
+print("Nodes Visited: ", maze.nodesVisited())
+print("Path: ", path)
